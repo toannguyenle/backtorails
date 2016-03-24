83512997a781c3284cc1224d394b3348ed481da9
toannguyenle <toan@me.com>
2016-03-24 10:40:48 -0700

    add more tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid sign up" do
@@ -25,5 +27,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
