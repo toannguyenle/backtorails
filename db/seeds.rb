d51f1b564aa40c42085fc907edcef7cda096ea5d
toannguyenle <toan@me.com>
2016-03-30 21:03:36 -0700

    Good stopping point for user activation. Come back later to reroute how non-activated user works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,35 @@
-User.create!(name:  "Toan",
-             email: "toan@toan.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+begin
+  User.create!(name:  "Toan",
+               email: "toan@toan.com",
+               password:              "foobar",
+               password_confirmation: "foobar",
+               admin: true,
+               activated: true,
+               activated_at: Time.zone.now)
+rescue
+  nil
+end
 
 100.times do |n|
-  name  = Faker::Name.name
-  email_name = name.split(" ")[1]
-  email = "#{email_name}@gmail.com"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+  begin
+    name  = Faker::Name.name
+    email_name = name.split(" ")[1]
+    email = "#{email_name}@gmail.com"
+    password = "password"
+    User.create!(name:  name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password,
+                 activated: true,
+                 activated_at: Time.zone.now)
+  rescue
+    email_name += n.to_s
+    email = "#{email_name}@gmail.com"
+    User.create!(name:  name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password,
+                 activated: true,
+                 activated_at: Time.zone.now)
+  end
 end
