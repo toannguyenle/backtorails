162500a46832023416ad53d3c5b02e4b45916ba3
toannguyenle <toan@me.com>
2016-04-07 10:37:28 -0700

    specify S3 bucket region

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,6 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -28,7 +29,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_limit: [400, 400]
   # process :scale => [200, 300]
   #
   # def scale(width, height)
